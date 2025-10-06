0a7fc5c41025b28fbb94c8e3d2faad0dead97534
Crow-1221 <hafidianwar284@gmail.com>
2025-10-06 04:02:16 +0100

    Update All Files

--- main.js
+++ main.js
@@ -1,48 +1,5 @@
-// let body = document.body
-// let navbar = document.querySelector(".navbar")
-// let modeBoard = document.querySelector(".mode-board")
-// let buttonMove = document.querySelector(".mode-btn")
-
-// let service1 = document.querySelector(".service-1")
-// let service2 = document.querySelector(".service-2")
-// let service3 = document.querySelector(".service-3")
-
-// let contact = document.querySelector(".contact")
-
-// modeBoard.onclick =  () => {
-//     body.classList.toggle("mode")
-//     navbar.classList.toggle("night-box")
-//     modeBoard?.classList.toggle("mode-board-night")
-//     buttonMove?.classList.toggle("btn-move")
-//     service1?.classList.toggle("night-box")
-//     service2?.classList.toggle("night-box")
-//     service3?.classList.toggle("night-box")
-//     contact?.classList.toggle("night-box")
-//     localStorage.setItem("mode", body.classList.contains('mode') ? 'dark' : 'light')
-// }
-// if (localStorage.getItem("mode") == "dark") {
-//     body.classList.add("mode")
-//     navbar.classList.add("night-box")
-//     modeBoard?.classList.add("mode-board-night")
-//     buttonMove?.classList.add("btn-move")
-//     service1?.classList.add("night-box")
-//     service2?.classList.add("night-box")
-//     service3?.classList.add("night-box")
-//     contact?.classList.add("night-box")
-
-// }
-// else {
-//     body.classList.remove("mode")
-//     navbar.classList.remove("night-box")
-//     modeBoard?.classList.remove("mode-board-night")
-//     buttonMove?.classList.remove("btn-move")
-//     service1?.classList.remove("night-box")
-//     service2?.classList.remove("night-box")
-//     service3?.classList.remove("night-box")
-//     contact?.classList.remove("night-box")
-// }
-
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     console.log("DOM Ready ")
-// })
+let hamburger = document.querySelector('.hamburger')
+let sidebar = document.querySelector('.sidebar')
+hamburger.addEventListener("click", () => {
+    sidebar.classList.toggle("active")
+})
